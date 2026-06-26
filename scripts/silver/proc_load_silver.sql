b40efdca8e59369140b8afb79ffa70c7a90cbb3a
Gunn <91450515+xcl0@users.noreply.github.com>
2026-06-26 15:37:01 +0700

    Create stored procedure to load data into silver schema
    This stored procedure loads data into the 'silver' schema from external CSV files, truncating existing tables and inserting new data from the 'bronze' schema.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,181 @@
+/*
 
+=====================================================================
+Stored Procedure: Load Silver Layer (Source -> Silver)
+=====================================================================
+
+Script Purpose:
+  This stored procedure loads data into the 'silver' schema from external CSV files.
+  It performs the following actions:
+  - Truncates the silver tables before loading data.
+  - Uses the 'bulk insert' command to load data from CSV files to silver tables.
+
+Parameters:
+  None.
+  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+  exec silver.load_bronze;
+=====================================================================
+*/
+
+create or alter procedure silver.load_silver as
+begin
+	print '=============================================';
+	print 'Loading Silver Layer';
+	print '=============================================';
+
+	print '---------------------------------------------';
+	print 'Loading CRM Tables';
+	print '---------------------------------------------';
+
+	print '>> Truncting Table: silver.crm_cust_info';
+	truncate table silver.crm_cust_info
+	print '>> Inserting Data Into: silver.crm_cust_info';
+	insert into silver.crm_cust_info (
+		cst_id,
+		cst_key,
+		cst_firstname,
+		cst_lastname,
+		cst_marital_status,
+		cst_gndr,
+		cst_create_date)
+
+	select
+		cst_id,
+		cst_key,
+		trim(cst_firstname) as cst_firstname,
+		trim(cst_lastname) as cst_lastname,
+		case when upper(trim(cst_marital_status)) = 'S' then 'Single'
+			 when upper(trim(cst_marital_status)) = 'M' then 'Married'
+			 else 'n/a'
+		end as cst_marital_status,
+		case when upper(trim(cst_gndr)) = 'F' then 'Female'
+			 when upper(trim(cst_gndr)) = 'M' then 'Male'
+			 else 'n/a'
+		end as cst_gndr,
+		cst_create_date
+		from (
+			select
+			*,
+			row_number() over (partition by cst_id order by cst_create_date desc) as flag_last
+			from bronze.crm_cust_info
+			where cst_id is not null
+		)t where flag_last = 1;
+
+
+	print '>> Truncting Table: silver.crm_prd_info';
+	truncate table silver.crm_prd_info
+	print '>> Inserting Data Into: silver.crm_prd_info';
+	insert into silver.crm_prd_info (
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+	)
+	select
+	prd_id,
+	replace(substring(prd_key,1,5),'-','_') as cat_id,
+	substring(prd_key,7,len(prd_key)) as prd_key,
+	prd_nm,
+	coalesce(prd_cost,0) as prd_cost,
+	case when upper(trim(prd_line)) = 'M' then 'Mountain'
+		 when upper(trim(prd_line)) = 'R' then 'Road'
+		 when upper(trim(prd_line)) = 'S' then 'Other Sales'
+		 when upper(trim(prd_line)) = 'T' then 'Touring'
+		else 'n/a'
+	end as prd_line,
+	cast(prd_start_dt as date) as prd_start_dt,
+	cast(lead(prd_start_dt) over (partition by prd_key order by prd_start_dt) -1 as date) as prd_end_dt
+	from bronze.crm_prd_info;
+
+
+	print '>> Truncting Table: silver.crm_sales_details';
+	truncate table silver.crm_sales_details
+	print '>> Inserting Data Into: silver.crm_sales_details';
+	insert into silver.crm_sales_details (
+		sls_ord_num ,
+		sls_prd_key ,
+		sls_cust_id ,
+		sls_order_dt ,
+		sls_ship_dt ,
+		sls_due_dt ,
+		sls_sales ,
+		sls_quantity ,
+		sls_price
+	)
+	select 
+		sls_ord_num,
+		sls_prd_key,
+		sls_cust_id,
+	case when sls_order_dt = 0 or len(sls_order_dt) <> 8 then null
+			else cast(cast(sls_order_dt as varchar) as date)
+	end as sls_order_dt,
+	case when sls_ship_dt = 0 or len(sls_ship_dt) <> 8 then null
+			else cast(cast(sls_ship_dt as varchar) as date)
+	end as sls_ship_dt,
+	case when sls_due_dt = 0 or len(sls_due_dt) <> 8 then null
+			else cast(cast(sls_due_dt as varchar) as date)
+	end as sls_due_dt,
+	case when sls_sales is null or sls_sales <= 0 or sls_sales <> sls_quantity * abs(sls_price)
+		then sls_quantity * abs(sls_price)
+			else sls_sales
+	end as sls_sales,
+		sls_quantity,
+	case when sls_price is null or sls_price <= 0
+		 then sls_sales / nullif(sls_quantity, 0)
+			else sls_price
+	end as sls_price
+	from bronze.crm_sales_details;
+
+	print '---------------------------------------------';
+	print 'Loading ERP Tables';
+	print '---------------------------------------------';
+
+	print '>> Truncting Table: silver.erp_cust_az12';
+	truncate table silver.erp_cust_az12
+	print '>> Inserting Data Into: silver.erp_cust_az12';
+	insert into silver.erp_cust_az12 (cid, bdate, gen)
+	select
+	case when cid like 'NA%' then substring(cid, 4, len(cid))
+		 else cid
+	end cid,
+	case when bdate > getdate() then null
+		else bdate
+	end as bdate,
+	case when upper(trim(replace(gen, char(13), ''))) IN ('F', 'FEMALE') then 'Female'
+		 when upper(trim(replace(gen, char(13), ''))) IN ('M', 'MALE') then 'Male'
+		 else 'n/a'
+	end as gen
+	from bronze.erp_cust_az12
+
+	print '>> Truncting Table: silver.erp_loc_a101';
+	truncate table silver.erp_loc_a101
+	print '>> Inserting Data Into: silver.erp_loc_a101';
+	insert into silver.erp_loc_a101
+	(cid, cntry)
+	select
+	replace(cid, '-', '') as cid,
+	case when trim(replace(cntry, char(13), '')) = 'DE' then 'Germany'
+		 when trim(replace(cntry, char(13), '')) in ('US', 'USA') then 'United States'
+		 when trim(replace(cntry, char(13), '')) = '' or cntry is null then 'n/a'
+		 else trim(replace(cntry, char(13), ''))
+	end as cntry
+	from bronze.erp_loc_a101;
+
+	print '>> Truncting Table: silver.erp_px_cat_g1v2';
+	truncate table silver.erp_px_cat_g1v2
+	print '>> Inserting Data Into: silver.erp_px_cat_g1v2';
+	insert into silver.erp_px_cat_g1v2
+	(id, cat, subcat, maintenance)
+	select
+	id,
+	cat,
+	subcat,
+	maintenance
+	from bronze.erp_px_cat_g1v2
+end
